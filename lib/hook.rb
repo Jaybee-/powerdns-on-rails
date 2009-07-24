34a89daa1ed52b35432108070c7cb4525f243f54
jjohnsen <jan.age.johnsen@gmail.com>
2009-07-24 14:57:04 +0200

    Reworked hooks code to use configuration

--- lib/hook.rb
+++ lib/hook.rb
@@ -1,28 +1,41 @@
 require 'rubygems'
+require 'yaml'
 
 module Hook
-  @@subclasses = []  
   @@hooks = {}
-
-  # Register subclass that contains response to hooks
-  def self.register_subclass(subclass)
-    @@subclasses << subclass  
-  end
-
-  # Execute hook on all subclasses that responds to it
+  @@config = nil
+  @@subclasses = nil
+  
+  # Execute hook on all registered subclasses
   def self.execute(hook, caller)
+
+    @@hooks[hook] ||= self.subclasses.select{|subclass| subclass.respond_to?(hook)}
+    
     responses = []
-    @@hooks[hook] ||= @@subclasses.select{|subclass| subclass.respond_to?(hook)}
     @@hooks[hook].each{ |m|
       responses << response = m.send(hook, caller)
       yield response if block_given?
     }
     responses
   end
-
-  class Base
-    def self.inherited(subclass)
-      Hook::register_subclass(subclass)
+  
+  # Find configurated subclasses that aren't disabled
+  def self.subclasses
+    @@subclasses ||= self.config.map do |subclass, config|
+      subclass.classify.constantize unless config["enabled"] == false
     end
+  end
+  
+  # Read config
+  def self.config
+    begin
+      @@config ||= YAML.load_file("#{RAILS_ROOT}/config/hooks.yml")
+    rescue
+      @@config = {}
+    end
+  end
+  
+  def self.config_for(subclass)
+    self.config[subclass] || {}
   end
 end
